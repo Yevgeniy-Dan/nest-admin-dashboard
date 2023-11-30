13007ffe445e6d1a10ad51139636154d4f4d42b4
Yevhenii Danoi <geneforapp@gmail.com>
2023-11-30 13:49:56 +0200

    mongoose and swagger setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import configuration from './config/configuration';
+import { Configuration } from './interfaces/configuration.interface';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      cache: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<Configuration>) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
